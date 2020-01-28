f1f8f2ae6f1e8050515ef084e159abd53c74e157
Dikker <37991420+Dikker@users.noreply.github.com>
2020-01-28 15:09:12 -0300

    Core assets
    core assets created

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public float speed = 2f;
+    public Vector2 direction;
+
+    public float livingTime = 3f;
+    void Start()
+    {
+        Destroy(this.gameObject, livingTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movent = direction.normalized * speed * Time.deltaTime;
+        this.transform.position = new Vector2(transform.position.x + movent.x, transform.position.y + movent.y);
+
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private Transform _firepoint;
+    public GameObject bulletPrefab;
+    public GameObject shooter;
+    Script bulletComponent;
+
+    private void Awake()
+    {
+        _firepoint = transform.Find("firepoint");
+
+    }
+
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if(bulletPrefab != null && _firepoint != null && shooter != null)
+        {
+            GameObject myBullet = Instantiate(bulletPrefab, _firepoint.position, Quaternion.identity) as GameObject;
+
+            bulletComponent = myBullet.GetComponent<Script>();
+
+            if (shooter.transform.localScale.x < 0)
+            {
+                //left
+                bulletComponent.direction = Vector2.left; // new Vector2(-1f, 0f)
+            }
+            else
+            {
+                //right
+                bulletComponent.direction = Vector2.right; //  new Vector2(1f, 0f)
+            }
+            
+        }
+    }
+}
